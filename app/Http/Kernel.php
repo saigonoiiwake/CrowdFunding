8a7f46135a43024357a22c26b91bbf5e4bc12965
John Fu <john80510@gmail.com>
2019-01-25 01:26:51 +0800

    1. teat csrf

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
     ];
 
     /**
@@ -40,7 +42,10 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\VerifyCsrfToken::class,
         ],
+
+        'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
