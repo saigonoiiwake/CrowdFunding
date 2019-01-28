34753bff580d818f5d47e7c955273740b52c94c7
黃惠農 <huang.h.af@m.titech.ac.jp>
2019-01-28 21:43:21 +0900

    add google

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -28,4 +28,52 @@ class ApiAuthController extends Controller
             'expires_in' => auth("api")->factory()->getTTL() * 60
         ]);
     }
+<<<<<<< HEAD
+=======
+
+    public function redirectToProvider($provider)
+    {
+       return Socialite::driver($provider)->stateless()->redirect();
+    }
+
+   public function handleProviderCallback($provider)
+    {
+        $userSocial = Socialite::driver($provider)->stateless()->user();
+        $token = $userSocial->token;
+        $refreshToken = $userSocial->refreshToken; // not always provided
+        $expiresIn = $userSocial->expiresIn;
+        
+        DB::beginTransaction();
+        try {
+
+          $data = [
+            'nick_name' => $userSocial->getName(),
+            'email' => $userSocial->getEmail(),
+            'avatar' => $userSocial->getAvatar(),
+            'password' =>  bcrypt(123456),
+          ];
+
+          $findUser = User::where('email', $userSocial->getEmail())->first();
+
+          if( $findUser )
+          {
+            \Auth::login($findUser);
+          }
+          else {
+            User::newUser($data);
+            $newUser = User::where('email', $userSocial->getEmail())->first();
+            \Auth::login($newUser);
+            DB::commit();
+          }
+
+
+        return $this->respondWithToken($token);
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+    }
+
+>>>>>>> 86a75f9... add google account
 }
